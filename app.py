b31ed27feb09be72a5559fc001516de6202dfe2f
aishiksarkar1-ui <aishiksarkar1@gmail.com>
2026-09-25 14:37:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,10 @@ def blog_share_market_tips():
 def blog_power_of_compounding_bn():
     return render_template('power-of-compounding-bn.html')
 
+@app.route('/learn/fundamental-analysis')
+def learn_fundamental_analysis():
+    return render_template('learnfolder/fundamental-analysis.html')
+
 
 
 
